13e7d2824af64f84fe86e7e31daea78f8f1dd95e
Simon Rodriguez <simon@hellowhiteout.com>
2019-01-09 18:38:37 +0100

    Changed so results show up in its own window. If nothing is found, a popup tells you.

--- Editor/AssetDependency.cs
+++ Editor/AssetDependency.cs
@@ -1,110 +1,174 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using System.Text;
 
-public class AssetDependency : AssetPostprocessor
+namespace UnityAssetDependency
 {
-	static string[] allGuids = null;
-	static Dictionary<string, List<string>> database = null;
-
-	static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+	public class AssetResultsWindow : EditorWindow
 	{
-		allGuids = null;
-		database = null;
-	}
+		Dictionary<string, HashSet<string>> allResults = new Dictionary<string, HashSet<string>>();
 
-	[MenuItem("Assets/Find Where Used In Project", false, 30)]
-	static void CheckUsage()
-	{
-		float start = Time.realtimeSinceStartup;
+		Vector2 scroll;
 
-		BuildDatabase();
-
-		StringBuilder sb = new StringBuilder();
-		sb.AppendLine("Search Report");
-		sb.AppendLine("");
-
-		UnityEngine.Object[] objs = Selection.objects;
-		foreach (var obj in objs)
+		public static void ShowWindow(Dictionary<string, HashSet<string>> allResults)
 		{
-			if (obj == null)
-				continue;
-			
-			string path = AssetDatabase.GetAssetPath(obj);
-			string guid = AssetDatabase.AssetPathToGUID(path);
-
-			sb.AppendFormat("Searching for {0}...\n", obj.name);
-
-			List<string> users = null;
-			if (database.TryGetValue(guid, out users))
-				foreach (var u in users)
-					sb.AppendFormat("Found in: {0}\n", AssetDatabase.GUIDToAssetPath(u));
-
-			sb.AppendLine();
+			var window = EditorWindow.GetWindow<AssetResultsWindow>();
+			window.titleContent = new GUIContent("Dependency");
+			window.allResults = allResults;
+			window.Show();
 		}
 
-		/*
-		Log.D("The following assets are not used:");
-		foreach (var guid in allGuids)
+		void OnGUI()
 		{
-			var path = AssetDatabase.GUIDToAssetPath(guid);
-			var type = AssetDatabase.GetMainAssetTypeAtPath(path);
+			string clicked = null;
 
-			if (type == typeof(UnityEditor.DefaultAsset))
-				continue;
+			scroll = EditorGUILayout.BeginScrollView(scroll);
+			EditorGUILayout.BeginVertical();
 
-			if (!database.ContainsKey(guid))
-				Log.D(path);
-		}
-		*/
-
-		float stop = Time.realtimeSinceStartup;
-
-		sb.AppendFormat("Elapsed time: {0}\n", (stop - start));
-
-		Debug.Log(sb.ToString());
-	}
-
-	static void BuildDatabase()
-	{
-		//Already built database.
-		if (allGuids != null && database != null)
-			return;
-
-		EditorUtility.DisplayProgressBar("Building database", "Caching dependencies..", 0f);
-
-		allGuids = AssetDatabase.FindAssets("t:object");
-
-		database = new Dictionary<string, List<string>>();
-
-		float step = 1f / (float)allGuids.Length;
-		float progress = 0f;
-
-		for (int i = 0; i < allGuids.Length; i++)
-		{
-			var guid = allGuids[i];
-			var dependencies = AssetDatabase.GetDependencies(AssetDatabase.GUIDToAssetPath(guid), false);
-
-			foreach (var dep in dependencies)
+			int index = 0;
+			foreach (var kvp in allResults)
 			{
-				var g = AssetDatabase.AssetPathToGUID(dep);
+				index++;
 
-				List<string> refs = null;
-				if (!database.TryGetValue(g, out refs))
-				{
-					refs = new List<string>();
-					database.Add(g, refs);
-				}
+				EditorGUILayout.LabelField(kvp.Key, EditorStyles.boldLabel);
 
-				refs.Add(guid);
+				EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+				EditorGUILayout.LabelField("Found in:");
+				foreach (var path in kvp.Value)
+					if (GUILayout.Button(path))
+						clicked = path;
+				EditorGUILayout.EndVertical();
+
+				EditorGUILayout.Space();
+				EditorGUILayout.Space();
 			}
 
-			progress += step;
+			EditorGUILayout.EndVertical();
+			EditorGUILayout.EndScrollView();
 
-			EditorUtility.DisplayProgressBar("Building database", "Caching dependencies..", progress);
+			EditorGUILayout.Space();
+
+			if (GUILayout.Button("Close"))
+				Close();
+
+			if (clicked != null)
+			{
+				var obj = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(clicked);
+				if (obj != null)
+					EditorGUIUtility.PingObject(obj);
+			}
+		}
+	}
+
+	public class AssetDependency : AssetPostprocessor
+	{
+		static string[] allGuids = null;
+		static Dictionary<string, List<string>> database = null;
+
+		static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+		{
+			allGuids = null;
+			database = null;
 		}
 
-		EditorUtility.ClearProgressBar();
+		[MenuItem("Assets/Find Where Used In Project", false, 30)]
+		static void CheckUsage()
+		{
+			//float start = Time.realtimeSinceStartup;
+
+			BuildDatabase();
+
+			Dictionary<string, HashSet<string>> allResults = new Dictionary<string, HashSet<string>>();
+
+			bool foundAnything = false;
+
+			UnityEngine.Object[] objs = Selection.objects;
+			foreach (var obj in objs)
+			{
+				if (obj == null)
+					continue;
+
+				HashSet<string> results = new HashSet<string>();
+
+				string path = AssetDatabase.GetAssetPath(obj);
+				string guid = AssetDatabase.AssetPathToGUID(path);
+
+				if (AssetDatabase.IsValidFolder(path))
+					continue;
+
+				List<string> users = null;
+				if (database.TryGetValue(guid, out users))
+					foreach (var u in users)
+						results.Add(AssetDatabase.GUIDToAssetPath(u));
+
+				foundAnything |= results.Count > 0;
+
+				allResults.Add(path, results);
+			}
+
+			/*
+			Log.D("The following assets are not used:");
+			foreach (var guid in allGuids)
+			{
+				var path = AssetDatabase.GUIDToAssetPath(guid);
+				var type = AssetDatabase.GetMainAssetTypeAtPath(path);
+
+				if (type == typeof(UnityEditor.DefaultAsset))
+					continue;
+
+				if (!database.ContainsKey(guid))
+					Log.D(path);
+			}
+			*/
+
+			//float stop = Time.realtimeSinceStartup;
+
+			if (foundAnything)
+				AssetResultsWindow.ShowWindow(allResults);
+			else
+				EditorUtility.DisplayDialog("AssetDependency", "Did not find anything that uses selected asset.", "OK");
+		}
+
+		static void BuildDatabase()
+		{
+			//Already built database.
+			if (allGuids != null && database != null)
+				return;
+
+			EditorUtility.DisplayProgressBar("Building database", "Caching dependencies..", 0f);
+
+			allGuids = AssetDatabase.FindAssets("t:object");
+
+			database = new Dictionary<string, List<string>>();
+
+			float step = 1f / (float)allGuids.Length;
+			float progress = 0f;
+
+			for (int i = 0; i < allGuids.Length; i++)
+			{
+				var guid = allGuids[i];
+				var dependencies = AssetDatabase.GetDependencies(AssetDatabase.GUIDToAssetPath(guid), false);
+
+				foreach (var dep in dependencies)
+				{
+					var g = AssetDatabase.AssetPathToGUID(dep);
+
+					List<string> refs = null;
+					if (!database.TryGetValue(g, out refs))
+					{
+						refs = new List<string>();
+						database.Add(g, refs);
+					}
+
+					refs.Add(guid);
+				}
+
+				progress += step;
+
+				EditorUtility.DisplayProgressBar("Building database", "Caching dependencies..", progress);
+			}
+
+			EditorUtility.ClearProgressBar();
+		}
 	}
 }
